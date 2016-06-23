ae543c4dc23b9218ebc89a6a22f6451aaa7ad085
Namvu <nam.vu@globalsign.com>
2016-06-23 19:05:53 +0900

    init

--- src/main/java/com/nalaan/kobitoclient/KobitoclientApplication.java
+++ src/main/java/com/nalaan/kobitoclient/KobitoclientApplication.java
@@ -5,13 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
 @EnableEurekaClient
+@RestController
 public class KobitoclientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KobitoclientApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
 	}
 }
